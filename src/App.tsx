95a3577670a02387830bd30b94b601335d5180e2
Nguyen Hoang <nguyenhoang@Nguyens-MacBook-Pro.local>
2024-12-09 00:52:21 +0700

    init: create base

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import MainLayout from './layout/MainLayout'
+import Home from './pages/Home'
+import NotFound from './pages/NotFound'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+    ],
+  },
+  {
+    path: '*',
+    element: <NotFound />,
+  },
+])
+
+function App() {
+  return <RouterProvider router={router} />
+}
+
+export default App
